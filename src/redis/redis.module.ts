03febad0e4a1388e15bfb241b7b5c471183643f3
张瑞 <2896402717@qq.com>
2024-10-19 00:21:55 +0800

    修改redis哨兵模式确保链接的是主节点

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -9,6 +9,7 @@ import { RedisModule } from '@nestjs-modules/ioredis';
         host: 'localhost', // Redis 服务器地址
         port: 6379, // Redis 端口
         db: 0, // 如果你需要使用特定的数据库的话
+        role: 'master',
       },
       url: '',
       type: 'single',
